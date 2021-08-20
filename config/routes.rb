5902a0e10b581647d0a16f69912ed5f35593f5a8
root-344 <michimasa8@gmail.com>
2021-08-20 17:55:43 +0900

    詳細、編集、削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  get "posts/index" => "posts#index"
  root to: 'posts#index'
- resources :posts, only: [:index, :new, :create]
+ resources :posts
  post "posts/create" => "posts#create"
+ post "posts/:id/update" => "posts#update"
+ post "posts/:id/destroy" => "posts#destroy"
 end
