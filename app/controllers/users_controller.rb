59fb59b5ce01b03e648ebcfe6330cd3fab475767
root-344 <michimasa8@gmail.com>
2021-08-27 18:39:10 +0900

    ログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+
   def index
     @users = User.all
   end
@@ -17,8 +20,9 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "新規登録完了しました"
-      redirect_to("users/#{@user.id}")
+      redirect_to("/users/#{@user.id}")
     else
       render("/users/new")
     end
@@ -41,5 +45,32 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+
+  def login_form
+    @users = User.all
+  end
+
+  def login
+    @users = User.all
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    @user = User.all
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+
 
 end
