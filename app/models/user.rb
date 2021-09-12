d7d02ab5e20f9c082b763ad5e20ec3b0e7cf4999
root-344 <michimasa8@gmail.com>
2021-09-12 18:49:49 +0900

    varitation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :first_name, {presence: true, length: {maximum: 10}}, format: {with: /\A[一-龥]+\z/}
-  validates :last_name, {presence: true, length: {maximum: 10}}, format: {with: /\A[一-龥]+\z/}
-  validates :first_name_kana, {presence: true, length: {maximum: 10}}, format: {with: /\A[ｧ-ﾝﾞﾟ]+\z/}
-  validates :last_name_kana, {presence: true, length: {maximum: 10}}, format: {with: /\A[ｧ-ﾝﾞﾟ]+\z/}
-  validates :nickname, {presence: true, uniqueness: true, length: {maximum: 20}}, format: {with: /\A[一-龥]+\z/}
-  validates :email, {presence: true, uniqueness: true}, format: {with: /\A\S+@\S+\.\S+\z/}
-  validates :password, {presence: true, uniqueness: true, length: { minimum: 8 }}, format: {with: /\A[a-zA-Z0-9]+\z/i} 
+  validates :first_name, presence: true, length: {maximum: 10}, format: {with: /\A[一-龥]+\z/}
+  validates :last_name, presence: true, length: {maximum: 10}, format: {with: /\A[一-龥]+\z/}
+  validates :first_name_kana, presence: true, length: {maximum: 10}, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, length: {maximum: 10}, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :nickname, presence: true, uniqueness: true, length: {maximum: 20}
+  validates :password, presence: true, uniqueness: true, length: { minimum: 8 }, format: {with: /\A[a-zA-Z0-9]+\z/i} 
   
   def posts
     return Post.where(user_id: self.id)
