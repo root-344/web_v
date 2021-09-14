8f4bd4501180a86afe0948f7aff910e400c0a244
root-344 <michimasa8@gmail.com>
2021-09-14 13:10:30 +0900

    before_action変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
   before_action :ensure_correct_user, {only: [:edit, :update]}
-  before_action :side_index, {except: :update}
+  before_action :side_index
   
   def index
   end
