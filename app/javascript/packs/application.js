1fb2474be21bb16bea214188967cefcd2ca71ce1
root-344 <michimasa8@gmail.com>
2021-09-17 21:05:05 +0900

    表示修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require('jquery')
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
-// require('script.js')
-import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
-import "script.js"
+require('jquery')
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('script.js')
 
-Rails.start()
-// Turbolinks.start()
-ActiveStorage.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
