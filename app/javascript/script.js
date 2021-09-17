72c806b59e68c3d28f1d67e551154ea29d393ebf
root-344 <michimasa8@gmail.com>
2021-09-17 20:32:16 +0900

    aprication.js変更

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,10 +1,4 @@
 $(function() {
-  $('#login-btn').click(function() {
-    $('#login-modal').fadeIn();
-  });
-  // $('#signup-btn').click(function() {
-  //   $('.signup-modal-wrapper').fadeIn();
-  // });
   $('.close-modal').click(function() {
     $('#login-modal').fadeOut();
     $('#signup-modal').fadeOut();
