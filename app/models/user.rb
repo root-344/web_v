d18fdcf4f4214f8212ba3ce8d8fcbf6f7059e753
root-344 <michimasa8@gmail.com>
2021-09-19 13:29:35 +0900

    with_options変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,31 +10,26 @@ class User < ApplicationRecord
     validates :nickname
     validates :email
     validates :password
+    with_options length: {maximum: 10} do
+      validates :first_name
+      validates :last_name
+      validates :first_name_kana
+      validates :last_name_kana
+      with_options format: {with: /\A[一-龥]+\z/, message: "は漢字で入力して下さい。"} do
+        validates :first_name
+        validates :last_name
+      end
+      with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力して下さい。"} do
+        validates :first_name_kana
+        validates :last_name_kana
+      end
+    end
+    with_options uniqueness: true do
+      validates :nickname, length: {maximum: 20}
+      validates :email
+      validates :password, length: { minimum: 8 }, format: {with: /\A[a-zA-Z0-9]+\z/i,  message: "は英数字で入力して下さい。"}   
+    end
   end
-
-  with_options length: {maximum: 10} do
-    validates :first_name
-    validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-
-  with_options format: {with: /\A[一-龥]+\z/, message: "は漢字で入力して下さい。"} do
-    validates :first_name
-    validates :last_name
-  end
-
-  with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力して下さい。"} do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-
-  with_options uniqueness: true do
-    validates :nickname, length: {maximum: 20}
-    validates :email
-    validates :password, length: { minimum: 8 }, format: {with: /\A[a-zA-Z0-9]+\z/i,  message: "は英数字で入力して下さい。"}   
-  end
-
 
 
   def posts
